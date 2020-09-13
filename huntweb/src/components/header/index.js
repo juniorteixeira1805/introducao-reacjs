e6b8550ede356b860c1d6f20e6bcdab7ae7f6a54
Gilvan Carlos Teixeira Junior <49037876+juniorteixeira1805@users.noreply.github.com>
2020-09-13 04:09:42 -0300

    criação do componente header

---
+++ huntweb/src/components/header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+//Importando CSS do desta pasta
+import "./style.css"
+
+//-- constante que retorna uma função
+const Header = () => ( 
+    <header id="main-header">JSHant</header>
+);
+
+export default Header;
