0b68dcb815805012f2dfca4be41bee8f44be9c90
Junior Teixeira <juniorteixeira1805@ufrn.edu.br>
2021-04-03 22:51:11 -0300

    projeto finalizado

--- huntweb/src/pages/main/index.js
+++ huntweb/src/pages/main/index.js
@@ -2,18 +2,38 @@ import React, {Component} from 'react'
 import api from "../../services/api"
 
 export default class Main extends Component {
+    state = {
+        products : [],
+    }
     //Componente que gera uma ação logo quando o componente é exibido em tela
     componentDidMount(){
         this.loadProducts();
     }
 
     loadProducts = async () => {
-        const response = await api.get('/products')
+        try {
+            const response = await api.get('/products')
 
-        console.log(response.data.docs)
+            this.setState({ products: response.data.docs })    
+        } catch (error) {
+            console.log(error)
+        }
     }
 
     render() {
-        return <h1>Olá Devorion</h1>
+        
+        const {products} = this.state
+
+        return (
+            <div className="product-list">
+                {products.map(product => (
+                    <article key={product._id}>
+                        <strong>{product.title}</strong>
+                        <p>{product.description}</p>
+                        <a href="">acessar</a>
+                    </article>
+                ))}
+            </div>
+        )
     }
 }
